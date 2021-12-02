3d6606ba6d3ecb2a332d648558f6991af4486be6
Mehmet Killioglu <mehmet.killioglu@unikie.com>
2021-12-02 18:24:32 +0200

    Build problems resolved, removed utils.hpp

--- include/ros2_gremsy/gremsy.hpp
+++ include/ros2_gremsy/gremsy.hpp
@@ -5,11 +5,14 @@
 #include <sensor_msgs/msg/imu.hpp>
 #include <geometry_msgs/msg/vector3_stamped.hpp>
 #include <geometry_msgs/msg/quaternion_stamped.hpp>
+#include <tf2_eigen/tf2_eigen.h>
 
+//#include "ros2_gremsy/utils.hpp"
 #include <../../gSDK/src/gimbal_interface.h>
 #include <../../gSDK/src/serial_port.h>
-#include "ros2_gremsy/utils.hpp"
 
+#define DEG_TO_RAD (M_PI / 180.0)
+#define RAD_TO_DEG (180.0 / M_PI)
 namespace ros2_gremsy
 {
 
@@ -91,7 +94,117 @@ private:
   bool pan_axis_stabilize_;
   // @brief Uses the yaw relative to the gimbal mount to prevent drift issues. Only a light stabilization is applied.
   bool lock_yaw_to_vehicle_;
+  rcl_interfaces::msg::ParameterDescriptor getParamDescriptor(
+    const std::string & name,
+    const std::string & description,
+    const uint8_t & type)
+  {
+    auto descriptor = rcl_interfaces::msg::ParameterDescriptor();
 
+    descriptor.name = name;
+    descriptor.description = description;
+    descriptor.type = type;
+    return descriptor;
+  }
+
+  rcl_interfaces::msg::ParameterDescriptor getParamDescriptor(
+    const std::string & name,
+    const std::string & description,
+    const uint8_t & type,
+    double from_value,
+    double to_value,
+    double step)
+  {
+    auto descriptor = rcl_interfaces::msg::ParameterDescriptor();
+
+    descriptor.name = name;
+    descriptor.description = description;
+    descriptor.type = type;
+    descriptor.floating_point_range.resize(1);
+    descriptor.floating_point_range[0].from_value = from_value;
+    descriptor.floating_point_range[0].to_value = to_value;
+    descriptor.floating_point_range[0].step = step;
+    return descriptor;
+  }
+
+  rcl_interfaces::msg::ParameterDescriptor getParamDescriptor(
+    const std::string & name,
+    const std::string & description,
+    const uint8_t & type,
+    int from_value,
+    int to_value)
+  {
+    auto descriptor = rcl_interfaces::msg::ParameterDescriptor();
+
+    descriptor.name = name;
+    descriptor.description = description;
+    descriptor.type = type;
+    descriptor.integer_range.resize(1);
+    descriptor.integer_range[0].from_value = from_value;
+    descriptor.integer_range[0].to_value = to_value;
+    descriptor.integer_range[0].step = 1;
+    return descriptor;
+  }
+
+  control_gimbal_mode_t convertIntGimbalMode(int mode)
+  { // Allows int access to the control_gimbal_mode_t struct
+    switch (mode) {
+      case 0: return GIMBAL_OFF;
+      case 1: return LOCK_MODE;
+      case 2: return FOLLOW_MODE;
+      default:
+        return GIMBAL_OFF;
+    }
+  }
+
+
+  control_gimbal_axis_input_mode_t convertIntToAxisInputMode(int mode)
+  { // Allows int access to the control_gimbal_axis_input_mode_t struct
+    switch (mode) {
+      case 0: return CTRL_ANGLE_BODY_FRAME;
+      case 1: return CTRL_ANGULAR_RATE;
+      case 2: return CTRL_ANGLE_ABSOLUTE_FRAME;
+      default:
+        return CTRL_ANGLE_ABSOLUTE_FRAME;
+    }
+  }
+  Eigen::Quaterniond convertYXZtoQuaternion(double roll, double pitch, double yaw)
+  {
+    Eigen::Quaterniond quat_abs(
+      Eigen::AngleAxisd(-DEG_TO_RAD * pitch, Eigen::Vector3d::UnitY()) *
+      Eigen::AngleAxisd(-DEG_TO_RAD * roll, Eigen::Vector3d::UnitX()) *
+      Eigen::AngleAxisd(DEG_TO_RAD * yaw, Eigen::Vector3d::UnitZ()));
+    return quat_abs;
+  }
+
+  sensor_msgs::msg::Imu convertImuMavlinkMessageToROSMessage(mavlink_raw_imu_t message)
+  {
+    sensor_msgs::msg::Imu imu_message;
+
+    // Set accelaration data
+    imu_message.linear_acceleration.x = message.xacc;
+    imu_message.linear_acceleration.y = message.yacc;
+    imu_message.linear_acceleration.z = message.zacc;
+
+    // Set gyro data
+    imu_message.angular_velocity.x = message.xgyro;
+    imu_message.angular_velocity.y = message.ygyro;
+    imu_message.angular_velocity.z = message.zgyro;
+
+    return imu_message;
+  }
+
+  geometry_msgs::msg::QuaternionStamped stampQuaternion(
+    geometry_msgs::msg::Quaternion quat,
+    std::string frame_id,
+    builtin_interfaces::msg::Time time)
+  {
+    geometry_msgs::msg::QuaternionStamped quat_stamped;
+    quat_stamped.header.frame_id = frame_id;
+    quat_stamped.header.stamp = time;
+    quat_stamped.quaternion = quat;
+    return quat_stamped;
+  }
 
 };
 
