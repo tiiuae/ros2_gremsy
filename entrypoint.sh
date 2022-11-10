24da6abe8cf63326d4e48acd590c102f76f99aea
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2022-11-10 14:15:19 +0200

    Add another wait because the first wait will handle SIGTERM
    Wrap the command which we expect to give error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 _term() {
     # FILL UP PROCESS SEARCH PATTERN HERE TO FIND PROPER PROCESS FOR SIGINT:
@@ -14,12 +14,22 @@ _term() {
     fi
     kill -s SIGINT $pid
 }
+# Use SIGTERM or TERM, does not seem to make any difference.
 trap _term SIGTERM
 
 ros-with-env ros2 launch ros2_gremsy ros_gremsy_gimbal.launch.py serial_port:=${GIMBAL_SERIAL_PORT} &
 child=$!
 
 echo "Waiting for pid $child"
+# * Calling "wait" will then wait for the job with the specified by $child to finish, or for any signals to be fired.
+#   Due to "or for any signals to be fired", "wait" will also handle SIGTERM and it will shutdown before
+#   the node ends gracefully.
+#   The solution is to add a second "wait" call and remove the trap between the two calls.
+# * Do not use -e flag in the first wait call because wait will exit with error after catching SIGTERM.
+set +e
+wait $child
+set -e
+trap - TERM
 wait $child
 RESULT=$?
 
